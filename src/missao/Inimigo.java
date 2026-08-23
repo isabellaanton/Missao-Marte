695c68896377e298b4c0a842050e624f40433f2e
Anderson (Crush) de Lima <Anderson.link.crush@hotmail.com>
2026-08-23 10:42:06 -0300

    APENAS a criação da classe inimigo, sem lógica

---
+++ src/missao/Inimigo.java
@@ -0,0 +1,23 @@
+package missao;
+
+public class Inimigo {
+    private int x;
+    private int y;
+
+    public Inimigo(int x, int y) {
+        x = x;
+        y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void moverAleatoriamente(int minX,int maxX,int minY,int maxY) {
+
+    }
+}
