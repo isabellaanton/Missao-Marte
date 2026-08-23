88700f40c373bfa34b9c0f696630d0c5a85f6d7e
Anderson (Crush) de Lima <Anderson.link.crush@hotmail.com>
2026-08-23 14:42:00 -0300

    Inimigo agora sabe dos limites e não sairá dos limites

--- src/missao/Inimigo.java
+++ src/missao/Inimigo.java
@@ -1,12 +1,14 @@
 package missao;
 
+import java.util.Random;
+
 public class Inimigo {
     private int x;
     private int y;
 
     public Inimigo(int x, int y) {
-        x = x;
-        y = y;
+        this.x = x;
+        this.y = y;
     }
 
     public int getX() {
@@ -18,6 +20,30 @@ public class Inimigo {
     }
 
     public void moverAleatoriamente(int minX,int maxX,int minY,int maxY) {
+        Random random = new Random();
+        int direcao = random.nextInt(4);
 
+        switch(direcao) {
+            case 0:
+                if (this.y > minY) {
+                    this.y--;
+                }
+                break;
+            case 1:
+                if (this.x < maxX) {
+                    this.x++;
+                }
+                break;
+            case 2:
+                if (this.y < maxY) {
+                    this.y++;
+                }
+                break;
+            case 3:
+                if (this.x > minX) {
+                    this.x--;
+                }
+                break;
+        }
     }
 }
