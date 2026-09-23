88d29010d4a7ade94f1ceda9333e0dcd53b9e7e8
Isabella Anton <isabellagaspar65@gmail.com>
2026-09-23 19:46:15 -0300

    refactor: RankingRepository interface methods and package

--- src/exercicio10/repository/RankingRepository.java
+++ src/exercicio10/repository/RankingRepository.java
@@ -1,10 +1,10 @@
-package exercicio10.repository;
+package solidexercicio10.repository;
 
-import exercicio10.model.RankingEntry;
 import java.util.List;
+import solidexercicio10.model.Dificuldade;
 
 public interface RankingRepository {
-    List<RankingEntry> carregar();
-    void salvar(List<RankingEntry> ranking);
-    void resetar();
+    void salvar(String nome, int pontos, Dificuldade dificuldade, int passageiros, long tempoSegundos);
+    List<RankingEntry> listar();
+    void limpar();
 }
