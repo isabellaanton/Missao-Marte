3bb5714d0f9742d5f4063c05cdfc2309a113fdc0
Isabella Anton <isabellagaspar65@gmail.com>
2026-09-23 00:15:54 -0300

    refactor: Asteroide class to remove interface implementation
    Removed the Posicionavel interface implementation from Asteroide class.

--- src/exercicio10/model/Asteroide.java
+++ src/exercicio10/model/Asteroide.java
@@ -1,7 +1,6 @@
-package exercicio10.model;
+package solidexercicio10.model;
 
-public final class Asteroide extends EntidadeMapa implements Posicionavel{
+public final class Asteroide extends EntidadeMapa {
     public Asteroide(int x, int y) { super(x, y); }
     @Override public char getSimbolo() { return '#'; }
 }
-
