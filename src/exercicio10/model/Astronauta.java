77b0e4a509d63189c23112dd8d2d294ea41e7083
Isabella Anton <isabellagaspar65@gmail.com>
2026-09-23 00:16:31 -0300

    refactor: change package name for Astronauta class

--- src/exercicio10/model/Astronauta.java
+++ src/exercicio10/model/Astronauta.java
@@ -1,8 +1,7 @@
-package exercicio10.model;
+package solidexercicio10.model;
 
 public final class Astronauta extends Passageiro {
     public Astronauta(String nome, int x, int y) { super(nome, "Astronauta", x, y); }
     @Override public int getPontuacao() { return 10; }
     @Override public char getSimbolo() { return 'T'; }
 }
-
