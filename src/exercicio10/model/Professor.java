e31bf6ba85f44b088bdbcd4060c176cf39b2cb44
Isabella Anton <isabellagaspar65@gmail.com>
2026-09-23 19:52:03 -0300

    refactor: Rename package from exercicio10 to solidexercicio10

--- src/exercicio10/model/Professor.java
+++ src/exercicio10/model/Professor.java
@@ -1,8 +1,7 @@
-package exercicio10.model;
+package solidexercicio10.model;
 
 public final class Professor extends Passageiro {
     public Professor(String nome, int x, int y) { super(nome, "Professor", x, y); }
     @Override public int getPontuacao() { return 15; }
     @Override public char getSimbolo() { return 'P'; }
 }
-
