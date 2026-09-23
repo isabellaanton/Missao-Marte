b8f2d2535032ab5fd321c3c9ef3cc1ce7cd14d3a
Isabella Anton <isabellagaspar65@gmail.com>
2026-09-23 19:49:34 -0300

    refactor: package name

--- src/exercicio10/model/Engenheiro.java
+++ src/exercicio10/model/Engenheiro.java
@@ -1,8 +1,7 @@
-package exercicio10.model;
+package solidexercicio10.model;
 
 public final class Engenheiro extends Passageiro {
     public Engenheiro(String nome, int x, int y) { super(nome, "Engenheiro", x, y); }
     @Override public int getPontuacao() { return 20; }
     @Override public char getSimbolo() { return 'E'; }
 }
-
